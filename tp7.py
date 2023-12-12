a2f414495bff17b974869332edac2bfe0bd692d0
louis-ab <147056896+louis-ab@users.noreply.github.com>
2023-12-12 14:31:55 +0100

    Update tp7.py

--- tp7.py
+++ tp7.py
@@ -1,11 +1,11 @@
-class divisionparzero(Exception):
-    pass
-class pasentier(Exception):
-    pass
-class expneg(Exception):
-    pass
-class irrationel(Exception):
-    pass
+class Divisionparzero(Exception):
+    """Divison par zéro."""
+class Pasentier(Exception):
+    """Le nombre devrait être entier mais ne l'est pas."""
+class Expneg(Exception):
+    """Exponentiation par un nombre négatif."""
+class Irrationel(Exception):
+    """Le résulatat est irrationel."""
 
 class Fraction:
     """Class representing a fraction and operations on it
@@ -18,23 +18,31 @@ class Fraction:
     def __init__(self,num=0, den=1):
         """This builds a fraction based on some numerator and denominator.
 
-        PRE : num est un nombre entier ; den est un nombre entier
+        PRE : -
         POST : Crée l'objet Fraction avec num/den si den != 0
-        RAISE : divisionparzero si den = 0
+        RAISES : Divisionparzero si den = 0, Pasentier si num et/ou den n'est pas un int
         """
         if den == 0:
-            raise divisionparzero("Le dénominateur est 0")
-        elif num%1 or den%1:
-            raise pasentier("Le numérateur et/ou le dénominateur n'es pas entier")
-        else:
-            self.num = num
-            self.den = den
+            raise Divisionparzero("Le dénominateur est 0")
+        if num%1 or den%1:
+            raise Pasentier("Le numérateur et/ou le dénominateur n'es pas entier")
+        self.num = num
+        self.den = den
 
     @property
     def numerator(self):
+        """Permet de retourner le numérateur de la fraction.
+        PRE : -
+        POST : Renvoie le numérateur de la fraction
+        """
         return self.num
+
     @property
     def denominator(self):
+        """Permet de retourner le dénominateur de la fraction.
+        PRE : -
+        POST : Renvoie le dénominateur de la fraction
+        """
         return self.den
 
 # ------------------ Textual representations ------------------
@@ -63,22 +71,23 @@ class Fraction:
         """
         n = self.num/self.den
         num = self.num
-        sign = (n >= 0)
+        sign = n>=0
         entier = 0
         if sign:
-            while num >= 1:
+            while num > 1:
                 num -= self.den
                 entier += 1
         else:
-            while num <= -1:
+            while num < -1:
                 num += self.den
                 entier -= 1
         if num == 0:
             return f"{entier}"
-        else:
-            return f"{entier} + {num}/{self.den}"
-                
-    
+        if entier == 0:
+            return f"{num}/{self.den}"
+        return f"{entier} + {num}/{self.den}"
+
+
 # ------------------ Operators overloading ------------------
 
     def __add__(self, other):
@@ -88,7 +97,7 @@ class Fraction:
          POST : renvoie leur somme sous forme d'un nouvel objet Fraction
          """
         return Fraction(self.num*other.den + other.num*self.den, self.den * other.den)
-        
+
 
     def __sub__(self, other):
         """Overloading of the - operator for fractions
@@ -113,10 +122,10 @@ class Fraction:
 
         PRE : other est un objet Fraction par lequel diviser l'objet
         POST : leur division qui est un objet Fraction
-        RAISE : divisionparzero si other = 0
+        RAISES : Divisionparzero si other = 0
         """
         if other.num == 0:
-            raise divisionparzero("Le dénominateur est 0")
+            raise Divisionparzero("Le dénominateur est 0")
         return self * Fraction(other.den,other.num)
 
     def __pow__(self, other):
@@ -124,31 +133,30 @@ class Fraction:
 
         PRE : other est un objet Fraction
         POST : self**other, une Fraction
-        RAISE : expneg si le nombre exponencé est négatif, irrationel si le résultat serait irrationel
+        RAISES : - Expneg si le nombre exponencé est négatif,
+                 - Irrationel si le résultat serait irrationel
         """
-        
+
         # (a/b)^(c/d) = (a/b)^c * (a/b)^(1/d) = (a^c * a^(1/d) / b^c * b^(1/d))
         if self.num < 0 or self.den < 0:
-            raise expneg("le nombre exponencé est négatif")
-        else:
-            a = pow(self.num,1/other.den)
-            b = pow(self.den,1/other.den)
-            if a%1 == 0 and b%1 == 0:
-                return Fraction(int(pow(self.num,other.num)*a), int(pow(self.den,other.num)*b))
-            else:
-                raise irrationel("le résultat est irrationel")
-        
-    
-    
-    def __eq__(self, other) : 
+            raise Expneg("le nombre exponencé est négatif")
+        a = pow(self.num,1/other.den)
+        b = pow(self.den,1/other.den)
+        if a%1 == 0 and b%1 == 0:
+            return Fraction(int(pow(self.num,other.num)*a), int(pow(self.den,other.num)*b))
+        raise Irrationel("le résultat est irrationel")
+
+
+
+    def __eq__(self, other):
         """Overloading of the == operator for fractions
-        
+
         PRE : other est un objet Fraction
         POST : True si il est égal à l'objet, False sinon
         """
-        return asMixedNumber(self) == asMixedNumber(other)
-        
-        
+        return self.as_mixed_number() == other.as_mixed_number()
+
+
     def __float__(self) :
         """Returns the decimal value of the fraction
 
@@ -156,7 +164,7 @@ class Fraction:
         POST : renvoie un float de valeur équivalente à l'objet Fraction
         """
         return self.num/self.den
-    
+
 # TODO : [BONUS] You can overload other operators if you wish (ex : <, >, ...)
 
 
@@ -188,8 +196,7 @@ class Fraction:
         """
         if self.den > 0:
             return self.num < self.den
-        else:
-            return self.num > -self.den
+        return self.num > -self.den
 
     def is_unit(self):
         """Check if a fraction's numerator is 1 in its reduced form
@@ -202,7 +209,8 @@ class Fraction:
     def is_adjacent_to(self, other) :
         """Check if two fractions differ by a unit fraction
 
-        Two fractions are adjacents if the absolute value of the difference between them is a unit fraction
+        Two fractions are adjacents if the absolute value of the difference between them is
+            a unit fraction
 
         PRE : other est une objet fraction
         POST : 1 si other et l'objet sont adjacents, 0 sinon
@@ -213,13 +221,24 @@ class Fraction:
 if __name__ == '__main__':
     test = Fraction(2,1)
     test2 = Fraction(4,1)
-    print(test2)
-    print(test.as_mixed_number())
-    print(test.is_adjacent_to(test2))
-    print(test2.is_proper())
-    print(test.is_integer())
+    print("Fraction : ", test2)
+    print("Mixed : ", test2.as_mixed_number())
+    print("Adjacents : ", test2.is_adjacent_to(test))
+    print("Proper : ", test2.is_proper())
+    print("Integer : ", test2.is_integer())
     test3 = test**test2
-    print(test3)
-    
+    print("Exp : ", test3)
+
+    print("")
+    test4 = Fraction(1,2)
+    test5 = Fraction(2,3)
+    print("Mixed : ", test4.as_mixed_number())
+    print("Sum : ", test4+test5)
+    print("Product : ", test4*test5)
+    print("Equal : ", test4 == test5)
+    print("Float : ", float(test5))
+    print("Unit : ", test5.is_unit())
+
+
     # division par 0:
-    test4 = Fraction(1,0)
+    test6 = Fraction(1,0)
